246316dffa69618e86ab571f124ad614c24e53ad
Сторожев Алексей Сергеевич <StorozhevAS@baltkam.ru>
2024-02-09 15:22:36 +0300

    3.0.1

--- coursephotozen/course/urls.py
+++ coursephotozen/course/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
 from . import views
 
+# urlpatterns = [
+#     path('', views.course, name='course')
+# ]
+
+from django.urls import path
+from . import views
+
+
 urlpatterns = [
-    path('', views.course, name='course')
+    path('stream/<int:pk>/', views.get_streaming_video, name='stream'),
+    path('<int:pk>/', views.get_video, name='video'),
+    path('', views.get_list_video, name='home'),
 ]
